016ed5f891a1fbf4beea16c4261df4c5d60086c0
yusufbayrakdar <sabri.bayrakdar@boun.edu.tr>
2022-02-26 22:27:33 +0300

    production url added

--- config/env-config.tsx
+++ config/env-config.tsx
@@ -6,7 +6,7 @@ const ENVIRONMENT = {
 const APP: { [key: string]: string } = {
   development: `http://localhost:5000`,
   staging: `https://bilsemiq-api.herokuapp.com`,
-  production: `https://bilsemiq-api.herokuapp.com`,
+  production: `http://bilsemai-env.eba-eypmvxsj.eu-central-1.elasticbeanstalk.com`,
 };
 export const activeEnvironment =
   process.env.NODE_ENV === "development"
