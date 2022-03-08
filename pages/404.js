1630b51b6baca1956357f37263bd66dc66908f3b
yusufbayrakdar <sabri.bayrakdar@boun.edu.tr>
2022-03-09 00:28:21 +0300

    not found page

---
+++ pages/404.js
@@ -0,0 +1,24 @@
+import React from "react";
+import Head from "next/head";
+import Image from "next/image";
+
+function NotFound() {
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Head>
+        <title>Sayfa Bulunamadı • 404</title>
+        <meta name="description" content="Sayfa bulunamadı" />
+        <link rel="icon" href="/ideas.png" />
+      </Head>
+      <Image src="/404.svg" height={400} width={400} />
+    </div>
+  );
+}
+
+export default NotFound;
